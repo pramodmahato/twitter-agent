c1f1d709aa9eb6c0c0be07163446cbe382e5b6d4
Pramod Mahato <pramodkrmahato@hotmail.com>
2020-07-10 23:39:53 +0530

    env variables added

--- views/public/main.js
+++ views/public/main.js
@@ -1,12 +1,12 @@
 var allData=[]
 var firebaseConfig = {
-  apiKey: "AIzaSyD2arB0eKXpdqy_vtQ7Nq3PP8ehsjXMliA",
-  authDomain: "richpanel-e71c9.firebaseapp.com",
-  databaseURL: "https://richpanel-e71c9.firebaseio.com",
-  projectId: "richpanel-e71c9",
-  storageBucket: "richpanel-e71c9.appspot.com",
-  messagingSenderId: "685382152016",
-  appId: "1:685382152016:web:0c4851aed033b4f92a0655"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 function fetchMessage(id){
